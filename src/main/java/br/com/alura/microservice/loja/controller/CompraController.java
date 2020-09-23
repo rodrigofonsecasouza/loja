571a58abe3e593162cbe93c1e3c28a660a5d6750
Rodrigo <fonsecar13@gmail.com>
2020-09-23 17:57:33 -0300

    commit inicial

---
+++ src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -0,0 +1,31 @@
+package br.com.alura.microservice.loja.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.microservice.loja.dto.CompraDTO;
+import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.service.CompraService;
+
+@RestController
+@RequestMapping("/compra")
+public class CompraController {
+	
+	@Autowired
+	private CompraService compraService;
+
+	@PostMapping
+	public Compra realizaCompra(@RequestBody CompraDTO compra) {
+		return compraService.realizaCompra(compra);
+	}
+	
+	@GetMapping("/{id}")
+	public Compra getById(@PathVariable Long id) {
+		return compraService.getById(id);
+	}
+}
